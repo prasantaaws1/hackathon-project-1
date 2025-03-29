7f0c02dcb67620f34d27966355e27d7eb937ce8b
pk9637 <beherapkb745@gmail.com>
2025-03-29 17:17:34 +0530

    Adding for ecs

--- main.tf
+++ main.tf
@@ -21,23 +21,23 @@ module "vpc" {
   # az_count    = "1"
 }
 
-module "ecs_app" {
-  source                       = "./modules/ecs"
-  ec2_task_execution_role_name = "EcsTaskExecutionRoleName"
-  ecs_auto_scale_role_name     = "EcsAutoScaleRoleName"
-  app_image                    = "167365792572.dkr.ecr.us-west-1.amazonaws.com/django-app:production"
-  app_port                     = 8000
-  app_count                    = 1
-  health_check_path            = "/"
-  fargate_cpu                  = "1024"
-  fargate_memory               = "2048"
-  aws_region                   = terraform.workspace
-  az_count                     = "2"
-  subnets                      = module.network.public_subnet_ids
-  sg_ecs_tasks                 = [module.security.ecs_tasks_security_group_id]
-  vpc_id                       = module.network.vpc_id
-  lb_security_groups           = [module.security.alb_security_group_id]
-}
+# module "ecs_app" {
+#   source                       = "./modules/ecs"
+#   ec2_task_execution_role_name = "EcsTaskExecutionRoleName"
+#   ecs_auto_scale_role_name     = "EcsAutoScaleRoleName"
+#   app_image                    = "167365792572.dkr.ecr.us-west-1.amazonaws.com/django-app:production"
+#   app_port                     = 8000
+#   app_count                    = 1
+#   health_check_path            = "/"
+#   fargate_cpu                  = "1024"
+#   fargate_memory               = "2048"
+#   aws_region                   = terraform.workspace
+#   az_count                     = "2"
+#   subnets                      = module.network.public_subnet_ids
+#   sg_ecs_tasks                 = [module.security.ecs_tasks_security_group_id]
+#   vpc_id                       = module.network.vpc_id
+#   lb_security_groups           = [module.security.alb_security_group_id]
+# }
 
 module "security" {
   source   = "./modules/security"
