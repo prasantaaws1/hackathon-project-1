fa66c8608043083b5fdd4f7bad40d5ce8f0f4526
pk9637 <beherapkb745@gmail.com>
2025-03-29 16:22:32 +0530

    Adding backend

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -10,7 +10,6 @@ resource "aws_vpc" "vpc" {
 }
 
 resource "aws_subnet" "private" {
-  count      = var.az_count
   cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 8, count.index)
   # availability_zone = data.aws_availability_zones.available.names[count.index]
   vpc_id = aws_vpc.vpc.id
@@ -23,7 +22,6 @@ resource "aws_subnet" "private" {
 
 # Create var.az_count public subnets, each in a different AZ
 resource "aws_subnet" "public" {
-  count      = var.az_count
   cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 8, var.az_count + count.index)
   # availability_zone       = data.aws_availability_zones.available.names[count.index]
   vpc_id                  = aws_vpc.vpc.id
@@ -54,20 +52,17 @@ resource "aws_route" "internet_access" {
 
 # Create a NAT gateway with an Elastic IP for each private subnet to get internet connectivity
 resource "aws_eip" "gw" {
-  count      = var.az_count
   domain     = "vpc"
   depends_on = [aws_internet_gateway.gw]
 }
 
 resource "aws_nat_gateway" "gw" {
-  count         = var.az_count
   subnet_id     = element(aws_subnet.public.*.id, count.index)
   allocation_id = element(aws_eip.gw.*.id, count.index)
 }
 
 # Create a new route table for the private subnets, make it route non-local traffic through the NAT gateway to the internet
 resource "aws_route_table" "private" {
-  count  = var.az_count
   vpc_id = aws_vpc.vpc.id
 
   route {
@@ -78,7 +73,6 @@ resource "aws_route_table" "private" {
 
 # Explicitly associate the newly created route tables to the private subnets (so they don't default to the main route table)
 resource "aws_route_table_association" "private" {
-  count          = var.az_count
   subnet_id      = element(aws_subnet.private.*.id, count.index)
   route_table_id = element(aws_route_table.private.*.id, count.index)
 }
