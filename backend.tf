410c60e5ef9eb6528a5115385086b2067ad45612
pk9637 <beherapkb745@gmail.com>
2025-03-29 15:55:46 +0530

    Fixing for dynamodb table lockid error

--- backend.tf
+++ backend.tf
@@ -7,8 +7,9 @@ resource "aws_s3_bucket" "mybucket" {
 
 
 resource "aws_dynamodb_table" "statelock" {
-  name     = "state-lock"
-  hash_key = "LockID"
+  name         = "state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
   attribute {
     name = "LockID"
